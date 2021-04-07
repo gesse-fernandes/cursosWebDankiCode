bfe583c63ee093bce7d8a6b8a474f7991541bcea
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-07 17:54:02 -0300

    carregamento dinamico MVC na classes Application

---
+++ Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+namespace dankiCode\controllers;
+  class HomeController
+  {
+    public function index()
+    {
+        echo "estou na home!";
+    }
+  }
+
+?>
---
+++ Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Views/pages/404.php
@@ -0,0 +1,1 @@
+<h2>Opps Página não existe!</h2>
