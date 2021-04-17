be04c25d66a6387a9b475ef09502014777b78a35
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-16 21:48:38 -0300

    add sistema de login de se auto logar verificar minha senha e email no banco de dados e me redirecionar pra Home

--- Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Controllers/HomeController.php
+++ Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Controllers/HomeController.php
@@ -11,6 +11,33 @@ namespace dankiCode\controllers;
         }
         else 
         {
+          if(isset($_POST['acao']))
+          {
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $verifica = \dankiCode\Mysql::connect()->prepare("SELECT * FROM usuarios where email = ?");
+            $verifica->execute(array($email));
+            if($verifica->rowCount() == 0)
+            {
+              //nao existe usuario
+              \dankiCode\Utilidades::alerta("Não existe nenhum usuario com este email");
+              \dankiCode\Utilidades::alerta(INCLUDE_PATH);
+            }
+            else{
+              $dados = $verifica->fetch();
+              $senhaBanco =  $dados['senha'];
+              if(\dankiCode\Bcrypt::check($senha,$senhaBanco)){
+                //user logado com sucesso
+                $_SESSION['login'] = $dados['email'];
+            \dankiCode\Utilidades::alerta("Logado com sucesso");
+            \dankiCode\Utilidades::alerta(INCLUDE_PATH);
+              }
+              else{
+            \dankiCode\Utilidades::alerta("senha incorreta");
+            \dankiCode\Utilidades::alerta(INCLUDE_PATH);
+              }
+            }
+          }
           //Redenrisa pra criar uma conta.
            \dankiCode\views\MainView::render("login");
         }
