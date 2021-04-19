488f520092e9687aef2d07186d854a723a35df95
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-18 21:47:14 -0300

    sistema de logout terminação do front end de amizade

--- Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Controllers/HomeController.php
+++ Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Controllers/HomeController.php
@@ -1,47 +1,65 @@
 <?php
-namespace dankiCode\controllers;
-  class HomeController
-  {
-    public function index()
-    {
-        if(isset($_SESSION['login']))
-        {
-        //Renderisa a home do usuario  
-          \dankiCode\views\MainView::render("home");
-        }
-        else 
-        {
-          if(isset($_POST['acao']))
-          {
-            $email = $_POST['email'];
-            $senha = $_POST['senha'];
-            $verifica = \dankiCode\Mysql::connect()->prepare("SELECT * FROM usuarios where email = ?");
-            $verifica->execute(array($email));
-            if($verifica->rowCount() == 0)
-            {
-              //nao existe usuario
-              \dankiCode\Utilidades::alerta("Não existe nenhum usuario com este email");
-              \dankiCode\Utilidades::alerta(INCLUDE_PATH);
-            }
-            else{
-              $dados = $verifica->fetch();
-              $senhaBanco =  $dados['senha'];
-              if(\dankiCode\Bcrypt::check($senha,$senhaBanco)){
-                //user logado com sucesso
-                $_SESSION['login'] = $dados['email'];
-              \dankiCode\Utilidades::alerta("Logado com sucesso");
-              \dankiCode\Utilidades::alerta(INCLUDE_PATH);
-              }
-              else{
-                \dankiCode\Utilidades::alerta("senha incorreta");
-               \dankiCode\Utilidades::alerta(INCLUDE_PATH);
-              }
-            }
-          }
-          //Redenrisa pra criar uma conta.
-           \dankiCode\views\MainView::render("login");
-        }
-    }
-  }
+	
+	namespace dankiCode\Controllers;
 
-?>
+	class HomeController{
+
+
+		public function index(){
+
+			if(isset($_GET['loggout'])){
+				session_unset();
+				session_destroy();
+
+				\dankiCode\Utilidades::redirect(INCLUDE_PATH);
+			}
+
+
+			if(isset($_SESSION['login'])){
+				//Renderiza a home do usuário.
+				\dankiCode\Views\MainView::render('home');
+			}else{
+				//Renderizar para criar conta.
+
+				if(isset($_POST['acao'])){
+					$login = $_POST['email'];
+					$senha = $_POST['senha'];
+
+					
+
+					//Verificar no banco de dados.
+
+					$verifica = \dankiCode\MySql::connect()->prepare("SELECT * FROM usuarios WHERE email = ?");
+					$verifica->execute(array($login));
+
+
+
+					
+					if($verifica->rowCount() == 0){
+						//Não existe o usuário!
+						\dankiCode\Utilidades::alerta('Não existe nenhum usuário com este e-mail...');
+						\dankiCode\Utilidades::redirect(INCLUDE_PATH);
+					}else{
+						$dados = $verifica->fetch();
+						$senhaBanco = $dados['senha'];
+						if(\dankiCode\Bcrypt::check($senha,$senhaBanco)){
+							//Usuário logado com sucesso
+							$_SESSION['login'] = $dados['email'];
+							$_SESSION['nome'] = explode(' ',$dados['nome'])[0];
+							\dankiCode\Utilidades::alerta('Logado com sucesso!');
+							\dankiCode\Utilidades::redirect(INCLUDE_PATH);
+						}else{
+							\DankiCode\Utilidades::alerta('Senha incorreta....');
+							\dankiCode\Utilidades::redirect(INCLUDE_PATH);
+						}
+					}
+					
+
+				}
+
+				\dankiCode\Views\MainView::render('login');
+			}
+
+		}
+
+	}
--- Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Views/pages/home.php
+++ Desenvolvimento web new 2.0/modulo_07/ProjetoSocial_2.0/dankiCode/Views/pages/home.php
@@ -1,47 +1,166 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login na Rede Social</title>
+    <!--ALTERAR TITULO-->
+    <title>Bem-vindo, <?php echo $_SESSION['nome']; ?></title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="<?php echo INCLUDE_PATH_STATIC ?>estilos/feed.css" rel="stylesheet">
+
+
 </head>
 
 <body>
+
     <section class="main-feed">
         <div class="sidebar">
             <div class="logo-sidebar">
-                <img src="<?php echo INCLUDE_PATH_STATIC ?>images/logodanki.svg" alt="">
+                <img src="<?php echo INCLUDE_PATH_STATIC ?>images/logodanki.svg" />
             </div>
-            <br>
+            <br />
             <div class="menu-sidebar">
                 <h4>Menu</h4>
-                <br>
-                <a href="#"><i class="fa fa-newspaper-o"></i> feed</a>
-                <a href="#"><i class="fa fa-user-o"></i> perfil</a>
-                <a href="#"><i class="fa fa-users"></i> amigos</a>
+                <br />
+                <a href="#"><i class="fa fa-newspaper-o" aria-hidden="true"></i> feed</a>
+                <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> perfil</a>
+                <a href="#"><i class="fa fa-users" aria-hidden="true"></i> amigos</a>
 
+                <a href="?loggout">loggout</a>
             </div>
         </div>
+        <!--sidebar-->
         <div class="feed">
-            <div class="feed-single-post">
-                <div class="feed-single-post-author">
-                    <div class="img-single-post-author">
-                        <!----imagem -->
+            <div class="feed-wraper">
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
                     </div>
-                    <h3>gesséx</h3>
-                    <span>horario post</span>
+                    <div class="feed-single-post-content">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis itaque optio eum placeat quas ipsa neque culpa aspernatur ad illo distinctio, alias asperiores hic odit, quam omnis iure architecto nobis.</p>
+                    </div>
                 </div>
-                <div class="feed-single-post-content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ipsa, illum unde ullam hic, explicabo a quisquam aut debitis tempore eum vel? Reprehenderit doloremque possimus architecto in numquam unde odit?</p>
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
+                    </div>
+                    <div class="feed-single-post-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis quisquam, suscipit ad earum in sequi repudiandae aperiam iusto ullam veritatis a tempora voluptatum neque at magnam vero illo ducimus blanditiis?
+                        </p>
+                        <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                    </div>
+                </div>
+
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
+                    </div>
+                    <div class="feed-single-post-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam iste architecto ab earum? Itaque soluta illum nemo perspiciatis excepturi debitis veniam obcaecati eligendi, provident error quam, commodi ab voluptatem.</p>
+                    </div>
+                </div>
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
+                    </div>
+                    <div class="feed-single-post-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate facilis ex quis at. Commodi ullam reiciendis atque reprehenderit quaerat sunt optio et recusandae? Rem consequuntur sapiente dolore impedit corporis error.</p>
+                    </div>
+                </div>
+
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
+                    </div>
+                    <div class="feed-single-post-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis velit suscipit, error est amet itaque minima animi, deleniti deserunt quia voluptatem fugit optio unde ullam neque eius veritatis, asperiores ipsum!</p>
+                    </div>
+                </div>
+
+                <div class="feed-single-post">
+                    <div class="feed-single-post-author">
+                        <div class="img-single-post-author">
+                            <!--todo:COLOCAR IMAGEM PLACEHOLDER-->
+                            <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                        </div>
+                        <div class="feed-single-post-author-info">
+                            <h3>Gessé</h3>
+                            <p>08:15 20/09/2020</p>
+                        </div>
+                    </div>
+                    <div class="feed-single-post-content">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta veritatis ut, voluptatem nihil atque praesentium fugit hic et harum obcaecati voluptatibus repellat rem exercitationem saepe modi ducimus recusandae neque eveniet.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="friends-request-feed">
+                <h4>Solicitações de amizade</h4>
+                <div class="friend-request-single">
+                    <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                    <div class="friend-request-single-info">
+                        <h3>gessex</h3>
+                        <p><a href="">Aceitar</a> | <a href="">Recusar</a></p>
+                    </div>
+                </div>
+                <div class="friend-request-single">
+                    <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                    <div class="friend-request-single-info">
+                        <h3>Gessex</h3>
+                        <p><a href="">Aceitar</a> | <a href="">Recusar</a></p>
+                    </div>
+                </div>
+                <div class="friend-request-single">
+                    <img src="<?php echo INCLUDE_PATH_STATIC ?>images/avatar.jpg" />
+                    <div class="friend-request-single-info">
+                        <h3>Gessex</h3>
+                        <p><a href="">Aceitar</a> | <a href="">Recusar</a></p>
+                    </div>
                 </div>
             </div>
         </div>
+        <!--feed-->
     </section>
+
+
 </body>
+
 
 </html>
