65c970cbdcf31da1c3dc00e58fde705340cb7238
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-22 00:08:28 -0300

    add table posts

--- DesenvolvimentoWebNovo/modulo_07/ProjetoSocial_2.0/DankiCode/db/redeSocial.sql
+++ DesenvolvimentoWebNovo/modulo_07/ProjetoSocial_2.0/DankiCode/db/redeSocial.sql
@@ -20,15 +20,16 @@ CREATE  table usuarios(
  status int(11)*/
 
 );
-/*INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `ultimo_post`, `img`) VALUES
+/*
+CREATE TABLE `posts` (
 
-(1, 'Guilherme Grillo', 'guilhermegrillo.13@gmail.com', '$2a$08$MTQwNDExODAxOTYwN2RhNOR5/AqYak4c6KSVIlkxE9Xfrr7QWRdq2', '2021-04-14 22:10:34', '607da5ad82b62.jpg'),
+  `id` int(11) NOT NULL,
 
-(2, 'William', 'gui_grillo13@hotmail.com', '$2a$08$NDIyMDExNDc5NjA0Nzk3OOwXZD/cxhWlx1AUQREZroTYBEvgNmZm2', '0000-00-00 00:00:00', ''),
+  `usuario_id` int(11) NOT NULL,
 
-(3, 'Joao Ferreira', 'joaozinho@hotmail.com', '$2a$08$NDIyMDExNDc5NjA0Nzk3OOwXZD/cxhWlx1AUQREZroTYBEvgNmZm2', '2021-04-12 15:41:16', ''),
+  `post` text NOT NULL,
 
-(4, 'Pedro', 'joaozinho1@gmail.com', '$2a$08$NDIyMDExNDc5NjA0Nzk3OOwXZD/cxhWlx1AUQREZroTYBEvgNmZm2', '2021-04-19 12:51:52', '607da75aa16bb.jpg'),
+  `date` datetime NOT NULL
 
-(5, 'Joao Lucas', 'joaozinho2@gmail.com', '$2a$08$NDIyMDExNDc5NjA0Nzk3OOwXZD/cxhWlx1AUQREZroTYBEvgNmZm2', '2021-04-12 15:41:39', '');*/
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;*/
 
